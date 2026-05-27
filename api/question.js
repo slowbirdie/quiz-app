fdbc6a136e86665b02650d770292690192fef0e9
slowbirdie <slowbirdie.top@outlook.com>
2026-05-27 11:25:10 +0800

    Update question.js

--- api/question.js
+++ api/question.js
@@ -1,11 +1,17 @@
+// question.js 增加 type 字段，对填空/编程题不返回 options
 const QUESTION_BANK = require('../question-bank.js');
 
 module.exports = (req, res) => {
   const randomIndex = Math.floor(Math.random() * QUESTION_BANK.length);
   const q = QUESTION_BANK[randomIndex];
-  res.status(200).json({
+  const response = {
     id: q.id,
+    type: q.type,
     question: q.question,
-    options: q.options
-  });
+  };
+  // 只有单选、多选、判断题返回选项数组，填空编程题前端无需选项数组
+  if (['single', 'multiple', 'truefalse'].includes(q.type)) {
+    response.options = q.options;
+  }
+  res.status(200).json(response);
 };
